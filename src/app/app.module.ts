3b79b21d93c89b973b0265dedd1e39e9f01ca45c
Georg <georg.strassberger@googlemail.com>
2023-02-17 17:02:23 +0100

    creating the Template for the (Reactive) RecipeEditForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule} from "@angular/forms";
+import { FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from "./header/header.component";
@@ -11,8 +11,8 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from "./shared/dropdown.directive";
-import {ShoppinglistService} from "./services/shoppinglist.service";
-import {AppRoutingModule} from "./app-routing.module";
+import { ShoppinglistService } from "./services/shoppinglist.service";
+import { AppRoutingModule } from "./app-routing.module";
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
@@ -34,6 +34,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     BrowserModule,
     NgbModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
   ],
   providers: [ShoppinglistService],
--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup } from '@angular/forms';
 import {ActivatedRoute, Params, Router} from '@angular/router';
+import {RecipeService} from "../../services/recipe.service";
 
 @Component({
   selector: 'app-recipe-edit',
@@ -7,16 +9,41 @@ import {ActivatedRoute, Params, Router} from '@angular/router';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent {
-  id?: number;
+  id!: number;
   editMode: boolean = false;
-  constructor(private route: ActivatedRoute){ }
+  recipeForm!: FormGroup;
+  constructor(private route: ActivatedRoute,
+              private recipeService:RecipeService){ }
 
   ngOnInit(){
     this.route.params.subscribe(
       (params:Params)=>{
         this.id = +params['id'];
         this.editMode = params['id'] != null;
+        this.initForm();
       }
     )
   }
+
+  onSubmit(){
+    console.log(this.recipeForm);
+  }
+  private initForm():void{
+    let recipeName:string = '';
+    let recipeImagePath:string = '';
+    let recipeDescription:string = '';
+    if(this.editMode){
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+    }
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imagePath': new FormControl(recipeImagePath),
+      'description': new FormControl(recipeDescription)
+    });
+  }
+
+
 }
